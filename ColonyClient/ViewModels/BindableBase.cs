cdc2803a4cc1baea3adac9b7f00839f453f19c8e
atsushiKAWAMORITA <atsushi.kawamorita@gmail.com>
2016-09-15 08:48:43 +0900

    update

---
+++ ColonyClient/ViewModels/BindableBase.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ColonyClient
+{
+	public abstract class BindableBase : INotifyPropertyChanged
+	{
+		/// <summary>
+		/// Occurs when property changed.
+		/// </summary>
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		/// <summary>
+		/// Sets the property.
+		/// </summary>
+		/// <returns><c>true</c>, if property was set, <c>false</c> otherwise.</returns>
+		/// <param name="storage">Storage.</param>
+		/// <param name="value">Value.</param>
+		/// <param name="propertyName">Property name.</param>
+		/// <typeparam name="T">The 1st type parameter.</typeparam>
+		protected bool SetProperty<T>(ref T storage, T value, [CallerMemberName] string propertyName = null)
+		{
+			if (object.Equals(storage, value)) return false;
+
+			storage = value;
+			this.OnPropertyChanged(propertyName);
+			return true;
+		}
+
+		/// <summary>
+		/// Ons the property changed.
+		/// </summary>
+		/// <param name="propertyName">Property name.</param>
+		protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+		{
+			var eventHandler = this.PropertyChanged;
+			if (eventHandler != null)
+			{
+				eventHandler(this, new PropertyChangedEventArgs(propertyName));
+			}
+		}
+	}
+}
+
---
+++ ColonyClient/ViewModels/RelayCommand.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace ColonyClient
+{
+	/// <summary>
+	/// Relay command.
+	/// </summary>
+	public class RelayCommand : ICommand
+	{
+		/// <summary>
+		/// The execute.
+		/// </summary>
+		private readonly Action _execute;
+		/// <summary>
+		/// The can execute.
+		/// </summary>
+		private readonly Func<bool> _canExecute;
+		/// <summary>
+		/// Occurs when can execute changed.
+		/// </summary>
+		public event EventHandler CanExecuteChanged;
+		/// <summary>
+		/// Initializes a new instance of the <see cref="T:ColonyClient.RelayCommand"/> class.
+		/// </summary>
+		/// <param name="execute">Execute.</param>
+		public RelayCommand(Action execute) : this(execute, null) { }
+		/// <summary>
+		/// Initializes a new instance of the <see cref="T:ColonyClient.RelayCommand"/> class.
+		/// </summary>
+		/// <param name="execute">Execute.</param>
+		/// <param name="canExecute">Can execute.</param>
+		public RelayCommand(Action execute, Func<bool> canExecute)
+		{
+			if (execute == null) throw new ArgumentException("Execute!");
+			_execute = execute;
+			_canExecute = canExecute;
+		}
+		/// <summary>
+		/// Cans the execute.
+		/// </summary>
+		/// <returns><c>true</c>, if execute was caned, <c>false</c> otherwise.</returns>
+		/// <param name="parameter">Parameter.</param>
+		public bool CanExecute(object parameter)
+		{
+			return _canExecute == null ? true : _canExecute();
+		}
+		/// <summary>
+		/// Execute the specified parameter.
+		/// </summary>
+		/// <param name="parameter">Parameter.</param>
+		public void Execute(object parameter)
+		{
+			_execute();
+		}
+		/// <summary>
+		/// Raises the can execute changed.
+		/// </summary>
+		public void RaiseCanExecuteChanged()
+		{
+			var handler = CanExecuteChanged;
+			if (handler != null)
+			{
+				handler(this, EventArgs.Empty);
+			}
+		}
+	}
+
+	/// <summary>
+	/// Relay command.
+	/// </summary>
+	public class RelayCommand<T> : ICommand
+	{
+		/// <summary>
+		/// The execute.
+		/// </summary>
+		private readonly Action<T> _execute;
+		/// <summary>
+		/// The can execute.
+		/// </summary>
+		private readonly Func<T, bool> _canExecute;
+		/// <summary>
+		/// Occurs when can execute changed.
+		/// </summary>
+		public event EventHandler CanExecuteChanged;
+		/// <summary>
+		/// Initializes a new instance of the <see cref="T:ColonyClient.RelayCommand`1"/> class.
+		/// </summary>
+		/// <param name="execute">Execute.</param>
+		public RelayCommand(Action<T> execute) : this(execute, null) { }
+		/// <summary>
+		/// Initializes a new instance of the <see cref="T:ColonyClient.RelayCommand`1"/> class.
+		/// </summary>
+		/// <param name="execute">Execute.</param>
+		/// <param name="canExecute">Can execute.</param>
+		public RelayCommand(Action<T> execute, Func<T, bool> canExecute)
+		{
+			if (execute == null) throw new ArgumentException("Execute!");
+			_execute = execute;
+			_canExecute = canExecute;
+		}
+		/// <summary>
+		/// Cans the execute.
+		/// </summary>
+		/// <returns><c>true</c>, if execute was caned, <c>false</c> otherwise.</returns>
+		/// <param name="parameter">Parameter.</param>
+		public bool CanExecute(object parameter)
+		{
+			return _canExecute == null ? true : _canExecute((T)parameter);
+		}
+		/// <summary>
+		/// Execute the specified parameter.
+		/// </summary>
+		/// <param name="parameter">Parameter.</param>
+		public void Execute(object parameter)
+		{
+			_execute((T)parameter);
+		}
+		/// <summary>
+		/// Raises the can execute changed.
+		/// </summary>
+		public void RaiseCanExecuteChanged()
+		{
+			var handler = CanExecuteChanged;
+			if (handler != null)
+			{
+				handler(this, EventArgs.Empty);
+			}
+		}
+	}
+}
+
