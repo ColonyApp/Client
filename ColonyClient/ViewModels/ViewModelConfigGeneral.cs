96e58cfaae81a769c9f8073d6d01d8b1e343fbe8
atsushiKAWAMORITA <atsushi.kawamorita@gmail.com>
2016-09-15 11:04:48 +0900

    update

--- ColonyClient/ViewModels/ViewModelConfigGeneral.cs
+++ ColonyClient/ViewModels/ViewModelConfigGeneral.cs
@@ -1,10 +1,59 @@
 ﻿using System;
+using Xamarin.Forms;
+using System.Windows.Input;
+
 namespace ColonyClient
 {
-	public class ViewModelConfigGeneral
+	public class ViewModelConfigGeneral : BindableBase
 	{
-		public ViewModelConfigGeneral()
+		//private ModelConfigGeneral model = new ModelConfigGeneral();
+
+		/// <summary>
+		/// The navigation.
+		/// </summary>
+		private INavigation _navigation;
+
+		/// <summary>
+		/// Gets the go to edit page.
+		/// </summary>
+		/// <value>The go to edit page.</value>
+		public ICommand GoToEditPage{ get; private set;}
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="T:ColonyClient.ViewModelConfigGeneral"/> class.
+		/// </summary>
+		/// <param name="navigation">Navigation.</param>
+		public ViewModelConfigGeneral(INavigation navigation)
 		{
+			_navigation = navigation;
+			this.GoToEditPage = new Command(async () =>
+			{
+				await _navigation.PushAsync(new ConfigEdit(UserInfo));
+			});
+		}
+
+		/// <summary>
+		/// The user info.
+		/// </summary>
+		private InfomationOfUser userInfo;
+		/// <summary>
+		/// Gets or sets the user info.
+		/// </summary>
+		/// <value>The user info.</value>
+		public InfomationOfUser UserInfo
+		{
+			get { return userInfo; }
+			set
+			{
+				if (userInfo != null)
+				{
+					userInfo.OldNickName = userInfo.NickName;
+					userInfo.OldMailAddress = userInfo.MailAddress;
+					userInfo.OldGroupName01 = userInfo.GroupName01;
+					userInfo = value;
+					SetProperty(ref this.userInfo, value);
+				}
+			}
 		}
 	}
 }
--- ColonyClient/Views/ConfigGeneral.xaml.cs
+++ ColonyClient/Views/ConfigGeneral.xaml.cs
@@ -8,28 +8,10 @@ namespace ColonyClient
 {
 	public partial class ConfigGeneral : ContentPage
 	{
-		InfomationOfUser userInfo = null;
 		public ConfigGeneral()
 		{
 			InitializeComponent();
-			userInfo = new InfomationOfUser
-			{
-				UserID = Guid.NewGuid(),
-				NickName = string.Empty,
-				MailAddress = string.Empty,
-				IsLogicalDelete = false,
-				GroupID01 = Guid.NewGuid(),
-				GroupName01 = string.Empty,
-				OldNickName = string.Empty,
-				OldMailAddress = string.Empty,
-				OldGroupName01 = string.Empty
-			};
-			BindingContext = userInfo;
-		}
-
-		public async void OnEditButtonClicked(object sender, EventArgs e)
-		{
-			await Navigation.PushAsync(new ConfigEdit(userInfo));
+			this.BindingContext = new ViewModelConfigGeneral(this.Navigation);
 		}
 	}
 }
